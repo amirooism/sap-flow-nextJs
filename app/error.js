7af3f4d01f13eb0918bf8c5832ec7bebffc3181a
amirooism <119577370+amirooism@users.noreply.github.com>
2025-03-19 18:02:56 +0330

    Handelling error
    adding a error compnenet in the rout app so any error occured it pops up

---
+++ app/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1> An error occured!</h1>
+      <p>Faild to fetch projects, Pleast try again later</p>
+    </main>
+  );
+}
