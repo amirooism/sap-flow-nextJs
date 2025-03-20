f0d39b1591c3a7800bdb2ad0f46240a10fdefa44
amirooism <119577370+amirooism@users.noreply.github.com>
2025-03-20 08:17:42 +0330

    Rendering product details in dynamaic routes (slug)

--- app/meals/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export default function DynamicRoutes({params}) {
-    return (
-        <main>
-            <h1>Daynamic routes are here</h1>
-            <p>{params.slug}</p>
-
-        </main>
-    )
-}
